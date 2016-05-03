991efa0e78223e1c110f2db685850b98ac1a80a1
nataliewest <natalie.west@serenaandlily.com>
2016-05-02 20:33:44 -0700

    FINAL_css_html

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Home
+	//when user hovers over the button they will annimate
+
+
+	//when the user scrolls down the page, the top header will remain at the top (sticky nav)
+
+
+//Recipe 
+	//when the user hovers over any of the links, the page will turn a different shade
+
+
+//decor
+	//when the user hovers over any of the links, the page will turn a different shade
+
+//ettiquette
+	//when the user hovers over any of the links, the page will turn a different shade
+
+
+	
